6485f2dadcc767de9bd86d423b4643e622f988eb
Samantha Doran <samanthadoran3@gmail.com>
2016-02-15 15:32:06 -0500

    Add an incomplete ppu structure to test reading and writing

--- src/ppu.nim
+++ src/ppu.nim
@@ -1,6 +1,16 @@
 type
+  ppuRegs* = enum
+    PPUCTRL,
+    PPUMASK,
+    PPUSTATUS,
+    OAMADDR,
+    OAMDATA,
+    PPUSCROLL,
+    PPUADDR,
+    PPUDATA
   PPU* = ref PPUObj
   PPUObj = object
+    regs*: array[PPUCTRL..PPUDATA, uint8]
 
 proc reset*(c: PPU) =
   discard
