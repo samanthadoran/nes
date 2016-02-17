ebad183170950e4d62a79f65ab3e5657450587d8
Samantha Doran <samanthadoran3@gmail.com>
2016-02-17 11:52:29 -0500

    Add some initialization code for the ppu

--- src/ppu.nim
+++ src/ppu.nim
@@ -10,10 +10,28 @@ type
     PPUDATA
   PPU* = ref PPUObj
   PPUObj = object
+    oddFrame*: bool
     regs*: array[PPUCTRL..PPUDATA, uint8]
+    oam*: array[256, uint8]
 
-proc reset*(c: PPU) =
-  discard
+proc reset*(p: PPU) =
+  p.regs[ppuRegs.PPUCTRL] = 0u8
+  p.regs[ppuRegs.PPUMASK] = 0u8
+  #Status
+  #OAM ADDR stays unchanged
+  #2005/2006 latch is cleared
+  p.regs[ppuRegs.PPUSCROLL] = 0u8
+  #PPU Addr is unchanged
+  p.regs[ppuRegs.PPUDATA] = 0u8
+  p.oddFrame = false
 
-proc powerOn*(c: PPU) =
-  discard
+proc powerOn*(p: PPU) =
+  p.regs[ppuRegs.PPUCTRL] = 0u8
+  p.regs[ppuRegs.PPUMASK] = 0u8
+  #Status
+  p.regs[ppuRegs.OAMADDR] = 0u8
+  #2005/2006 latch is cleared
+  p.regs[ppuRegs.PPUSCROLL] = 0u8
+  p.regs[ppuRegs.PPUADDR] = 0u8
+  p.regs[ppuRegs.PPUDATA] = 0u8
+  p.oddFrame = false
